8d2346584acc174f2efac37bb1c324532d7bdcf4
Sarlini_089 <103440090+sarlini027@users.noreply.github.com>
2022-10-06 22:50:37 +0700

    Add files via upload

---
+++ Data.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Informasi Mahasiswa</title>
+    <style>
+    table {
+        border: 2px solid black;
+        position: absolute;
+        left: 50%;
+        top: 30%;
+background-color: rgb(127, 255, 244);
+    }
+   
+    h2 {
+        position: absolute;
+        left: 50%;
+        top: 15%;
+        background-color: darkgrey;
+        width: 175px;
+        height: 40px;
+        font-family: 'Times New Roman', Times, serif;
+    }
+</style>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td align="center">E-mail</td>
+            <td align="center">NIM</td>
+        </tr>
+
+        <tr>
+            <td>sarlini.120140089@student.itera.ac.id</td>
+            <td>120140089</td>
+        </tr> 
+
+
+    
+        
+    </table>
+    
+</body>
+</html>
